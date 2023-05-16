f85db97c5cd674a3c96969ddfca29683056d0591
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-05-15 23:42:22 -0300

    Update emPython.py

--- emPython.py
+++ emPython.py
@@ -2,7 +2,7 @@ from random import randint
 from time import sleep
 
 def jogarNovamente():
-  rsp = str(input('\nDeseja jogar novamente? (s/n) > '))
+  rsp = str(input('\nDeseja jogar novamente? (s/n)> '))
   if rsp.lower() == 'n':
     print('\nFoi um prazer jogar com você, {}. Até breve! 😉'.format(nome.capitalize()))
     return False
@@ -10,9 +10,18 @@ def jogarNovamente():
     print('\nOBA! Vamos mais uma! 🤩')
     return True
 
+def placar():
+  print('\n------ PLACAR -------')
+  print('Máquina: {} ponto(s)'.format(ptsPC))
+  print('{}: {} ponto(s)'.format(nome.capitalize(), ptsPlayer))
+  print('---------------------')
+
+ptsPC = 0
+ptsPlayer = 0
+
 executando = True
 
-nome = str(input('Seja bem-vindo(a) ao nosso jogo de adivinhação! Vamos brincar? 😄 \n\nMas antes diga-me qual o seu nome > '))
+nome = str(input('Seja bem-vindo(a) ao nosso jogo de adivinhação! Vamos brincar? 😄\n\nMas antes diga-me qual o seu nome > '))
 print()
 
 print('-='*40)
@@ -23,7 +32,7 @@ while executando:
   pc = randint(0, 10)
   player = int(input('\nMe diga: que número você acha que eu escolhi? '))
 
-  print('\nEntão você acha que é {}? ... 🤔'.format(player))
+  print('\nEntão você acha que é {}? 🤔'.format(player))
   sleep(3)
   
   print('\nVamos ver... ⏳')
@@ -31,7 +40,11 @@ while executando:
 
   if player == pc:
     print('\nPARABÉNS! 🥳 Você me venceu. Não fui o melhor dessa vez. 🫤')
+    ptsPlayer += 1
+    placar()
   else:
     print('\nERRADO! VENCI VOCÊ! 😎 O número que escolhi foi {} e não {}. Quem sabe na próxima. 🤷'.format(pc, player))
+    ptsPC += 1
+    placar()
 
   executando = jogarNovamente()
